3052b6e54d7b621c874065c220200a3edab65b0f
Mihail Slavchev <mihail.slavchev@telerik.com>
2015-07-13 11:26:56 +0300

    clean code

--- src/JavaTSAPI/src/com/telerik/Main.java
+++ src/JavaTSAPI/src/com/telerik/Main.java
@@ -20,6 +20,12 @@ import com.telerik.javascript.api.*;
  * argument.
  */
 public class Main {
+	
+	public static enum Input
+	{
+		Reflection,
+		SourceCode
+	}
 
     /**
      * The main method accepts the file(s) for verification. Multiple files can
@@ -32,73 +38,51 @@ public class Main {
      *            The java source files to be verified.
      */
 	public static void main(String[] args) {
-		main3(args);
+		
+		Input input = Input.SourceCode;
+		
+		switch (input)
+		{
+			case Reflection:
+				processUsingReflection(args);
+				break;
+				
+			case SourceCode:
+				processUsingSourceCode(args);
+				break;
+
+			default:
+				throw new RuntimeException("Not supported input:" + input);
+		}
 	}
 	
-    private static void main1(String[] args) {
-    	//if (args == null) {// || args.length == 4) {
-    	//System.setProperty("java.home", "C:\\Program Files\\Java\\jdk1.8.0_45");
-    		String jdkPath = "C:\\Program Files\\Java\\jdk1.8.0_45";
-    		String targetXml = "C:\\temp\\AppCompat_21_JavaDoc.xml";
-    		String[] directories =
-    			{
-    				"C:\\Dev\\adt\\sdk\\platforms\\android-21\\android.jar",
-    				"C:\\Dev\\adt\\sdk\\extras\\android\\support\\v7\\appcompat\\libs\\android-support-v4.jar",
-    				"C:\\Dev\\adt\\sdk\\extras\\android\\support\\v7\\appcompat\\libs\\android-support-v7-appcompat.jar"
-    			};
-    		//buildFromSource(jdkPath, targetXml, directories);
-    		buildFromSource(jdkPath, targetXml, directories);
-    		
-//    	} 
-//    	else if (args == null || args.length != 4) {
-//    		System.out.print("Usage:");
-//    		System.out.print(" -compile <jdk path> <target xml> < ; delimited directories list>");    		
-//    	} else if (args.length == 4 && args[0].equals("-compile")) {
-//    		String jdkPath = args[1];
-//    		String targetXml = args[2];
-//    		String[] directories = args[3].split(";");
-//    		buildFromSource(jdkPath, targetXml, directories);
-//    	}
+    private static void processUsingReflection(String[] args) {
+		String jdkPath = "C:\\Program Files\\Java\\jdk1.8.0_45";
+		String targetXml = "C:\\temp\\AppCompat_21_JavaDoc.xml";
+		String[] directories =
+		{
+			"C:\\Dev\\adt\\sdk\\platforms\\android-21\\android.jar",
+			"C:\\Dev\\adt\\sdk\\extras\\android\\support\\v7\\appcompat\\libs\\android-support-v4.jar",
+			"C:\\Dev\\adt\\sdk\\extras\\android\\support\\v7\\appcompat\\libs\\android-support-v7-appcompat.jar"
+		};
+		buildUsingReflection(jdkPath, targetXml, directories);
     }
     
-    public static void main3(String[] args) {
-    	//if (args == null || args.length == 4) {
-    		String jdkPath = "C:\\Program Files\\Java\\jdk1.8.0_31";
-    		String targetXml = "C:\\Work\\xPlatCore\\Android_XMLAPI_17_JavaDoc.xml";
-    		String[] directories =
-    			{ "C:\\Dev\\adt\\sdk\\sources\\android-17\\android",
-    			  "C:\\Dev\\adt\\sdk\\sources\\android-17\\java",
-    			  "C:\\Dev\\adt\\sdk\\sources\\android-17\\javax",
-    			  "C:\\Dev\\adt\\sdk\\sources\\android-17\\org"
-    			};
-    		buildFromSource3(jdkPath, targetXml, directories);
-        	HashSet<Character> c = com.telerik.javascript.api.JSToXMLPrinter.codemap;
-        	char[] a = new char[1];
-        	for (Character cc: c)
-        	{
-        		a[0] = cc;
-        		System.out.println(cc + "::" + Character.codePointAt(a, 0));
-        	}
-        	c = null;
-    	//} else 
-    	
-    	
-//		if (args == null || args.length != 4) {
-//    		System.out.print("Usage:");
-//    		System.out.print(" -compile <jdk path> <target xml> < ; delimited directories list>");    		
-//    	} else if (args.length == 4 && args[0].equals("-compile")) {
-//    		String jdkPath = args[1];
-//    		String targetXml = args[2];
-//    		String[] directories = args[3].split(";");
-//    		buildFromSource(jdkPath, targetXml, directories);
-//    	}
+    public static void processUsingSourceCode(String[] args) {
+		String jdkPath = "C:\\Program Files\\Java\\jdk1.8.0_31";
+		String targetXml = "C:\\Work\\xPlatCore\\Android_XMLAPI_17_JavaDoc.xml";
+		String[] directories =
+			{ "C:\\Dev\\adt\\sdk\\sources\\android-17\\android",
+			  "C:\\Dev\\adt\\sdk\\sources\\android-17\\java",
+			  "C:\\Dev\\adt\\sdk\\sources\\android-17\\javax",
+			  "C:\\Dev\\adt\\sdk\\sources\\android-17\\org"
+			};
+		buildUsingSourceCode(jdkPath, targetXml, directories);
     }
     
-	private static void buildFromSource3(String javaHome, String targetXml, String[] directories) {
+	private static void buildUsingSourceCode(String javaHome, String targetXml, String[] directories) {
 		System.out.println("Collecting class files...");
     	String xml = targetXml;
-    	
-    	//System.setProperty("java.home", javaHome);
     	
         try {
             CodeAnalyzerController controller = new CodeAnalyzerController();
@@ -151,35 +135,11 @@ public class Main {
 	}
 
 
-	private static void buildFromSource(String javaHome, String targetXml, String[] directories) {
-//		System.out.println("Collecting class files...");
+	private static void buildUsingReflection(String javaHome, String targetXml, String[] directories) {
     	String xml = targetXml;
-//    	
-//    	
-//    	System.setProperty("java.home", "C:\\Program Files\\Java\\jdk1.8.0_45");
-//    	
-//        try {
-//            CodeAnalyzerController controller = new CodeAnalyzerController();
-//            List<File> files = new ArrayList<File>();
-//            for (String dir: directories) {
-//            	addFromFolder(files, dir);	
-//            }
-//
-//            System.out.println("Java compiler processing classes to Java AST...");
-//			controller.invokeProcessor(files);
-//
-//        } catch (Throwable t) {
-//            t.printStackTrace();
-//        }
-//        
-//        System.out.println("Converting to TypeScript AST...");
-//            
-//        Map<String, JClass> map = ClassModelMap.getInstance().getClassInfoMap();
-//        Map<String, JClass> rootMap = ClassModelMap.getInstance().getRootClassInfoMap();
 
     	try {
 
-//        	JContext root = new ContextInfo(map, rootMap); // JavaJarContext.fromFile(jar);
     		JContext root = JavaJarContext.fromFile(directories);
         	
 			JToJSConverter converter = new JToJSConverter();
