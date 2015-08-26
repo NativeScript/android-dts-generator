5549e0f997debcbf073f72d64cd186ac4211ca26
Mihail Slavchev <mihail.slavchev@telerik.com>
2015-08-26 14:51:34 +0300

    test parser

--- src/JavaTSAPI/src/com/telerik/Main.java
+++ src/JavaTSAPI/src/com/telerik/Main.java
@@ -1,13 +1,23 @@
 package com.telerik;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.util.*;
 
+import javax.management.RuntimeErrorException;
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
 import javax.xml.transform.*;
 import javax.xml.transform.stream.*;
 
+import org.apache.bcel.classfile.Attribute;
+import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.Field;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.Method;
+import org.apache.bcel.classfile.Signature;
+
+import com.sun.tools.internal.ws.processor.generator.Names;
 import com.telerik.java.api.*;
 import com.telerik.java.api.compiler.model.*;
 import com.telerik.java.api.compiler.processor.*;
@@ -26,6 +36,330 @@ public class Main {
 		Reflection,
 		SourceCode
 	}
+	
+	
+	static class DtsWriter implements com.telerik.java.api.bcel.Generator.VisitorCallback
+	{
+		private int level = 0;
+		
+		private PrintStream output;
+		
+		public DtsWriter() throws FileNotFoundException
+		{
+			output = new PrintStream(new File("C:/Work/TS/TypeScriptHTMLApp1/test.d.ts"));
+		}
+		
+		@Override
+		public void onEnter(String packageName) {
+			String ident = new String(new char[level++]).replace("\0", "\t");
+			if (level == 1)
+			{
+				println(ident + "declare module " + packageName + " {");				
+			}
+			else
+			{
+				println(ident + "export module " + packageName + " {");
+			}
+		}
+
+		@Override
+		public void onLeave(String packageName) {
+			String ident = new String(new char[--level]).replace("\0", "\t");
+			println(ident + "}");
+		}
+
+		@Override
+		public void onVisit(Field field) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onVisit(Method method) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onEnter(JavaClass javaClass) {
+			String ident;
+			String name;
+			String className = javaClass.getClassName();
+			String packageName = javaClass.getPackageName();
+			int lastDollarSign = className.lastIndexOf("$");
+			boolean isNested = lastDollarSign > 0;
+			String[] outerClasses = null;
+			if (isNested)
+			{
+				name = className.substring(lastDollarSign + 1);
+				outerClasses = className.substring(packageName.length() + 1, lastDollarSign).split("\\.");
+				for (String outer: outerClasses)
+				{
+					ident = new String(new char[level++]).replace("\0", "\t");
+					println(ident + "export module " + outer + " {");
+				}
+			}
+			else
+			{
+				name = className.substring(packageName.length() + 1);
+			}
+			ident = new String(new char[level++]).replace("\0", "\t");
+			
+			Attribute[] attributes = javaClass.getAttributes();
+			String sig = "";
+			for (Attribute attribute : attributes)
+			{
+			    if (attribute instanceof Signature)
+			    {
+			        Signature signature = (Signature) attribute;
+			        int len = signature.getLength();
+			        boolean isGenerics = len > 0;
+			        if (isGenerics)
+			        {
+			        	System.out.println("class=" + javaClass.getClassName() + " signature=" + signature.getSignature());
+			        }
+			        SignatureParser sigParser = new SignatureParser(signature);
+			        sig = sigParser.parse();
+			        break;
+			    }
+			}
+			
+			println(ident + "export class " + name + sig + " extends " + getSupperClassName(javaClass) + " {");
+		}
+
+		@Override
+		public void onLeave(JavaClass javaClass) {
+			String ident = new String(new char[--level]).replace("\0", "\t");
+			println(ident + "}");
+
+			String className = javaClass.getClassName();
+			int lastDollarSign = className.lastIndexOf("$");
+			boolean isNested = lastDollarSign > 0;
+			if (isNested)
+			{
+				String packageName = javaClass.getPackageName();
+				String[] outerClasses = className.substring(packageName.length() + 1, lastDollarSign).split("\\.");
+				for (String outer: outerClasses)
+				{
+					ident = new String(new char[--level]).replace("\0", "\t");
+					println(ident + "}");
+				}
+			}
+		}
+		
+		private void println(String s)
+		{
+			output.println(s);
+		}
+		
+		private String getSupperClassName(JavaClass javaClass)
+		{
+			String name = javaClass.getSuperclassName();
+			
+			File file = new File(javaClass.getFileName());
+			
+			String[] packageNames = javaClass.getPackageName().split("\\.");
+			int len = packageNames.length + 1;
+			for (int i=0; i<len; i++)
+			{
+				file = file.getParentFile();
+			}
+			
+			String[] parts = name.split("\\.");
+			for (int i=0; i<parts.length; i++)
+			{
+				String p = parts[i];
+				if (i == (parts.length - 1))
+				{
+					p += ".class";
+				}
+				File f = new File(file, p);
+				if (!f.exists())
+				{
+					break;
+					
+				}
+				file = f;
+			}
+			
+			if (file.exists() && file.isFile())
+			{
+				String fileName = file.getAbsolutePath();
+				try
+				{
+					ClassParser cp = new ClassParser(fileName);
+					JavaClass clazz = cp.parse();
+					if (clazz.isPublic())
+					{
+						name = clazz.getClassName();
+					}
+					else
+					{
+						name = getSupperClassName(clazz);
+					}
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+			
+			return name.replace("$", ".");
+		}
+		
+		static class SignatureParser
+		{
+			private final Signature signature;
+			
+			private final String sig;
+			
+			private int idx;
+
+			public SignatureParser(Signature signature)
+			{
+				this.signature = signature;
+				this.sig = signature.getSignature();
+			}
+			
+			
+			public String parse()
+			{
+				idx = 1;
+				
+				System.out.print("<");
+				{
+					readArgList(0);
+				}
+				System.out.print(">");
+				
+				System.out.println();
+				System.out.println();
+				
+				return sig;
+			}
+			
+			private void readArgList(int paramIdx)
+			{
+				if (paramIdx > 0)
+				{
+					System.out.print(", ");
+				}
+
+				
+				readArg();
+				
+				assert sig.charAt(idx-1) == ';';
+				
+				char c = sig.charAt(idx);
+				if (c != '>')
+				{
+					readArgList(paramIdx + 1);
+				}
+			}
+			
+			private void readArg()
+			{
+				readId();
+				
+				assert sig.charAt(idx) == ':';
+				
+				readExtends2();
+				
+				assert sig.charAt(idx) != ':';
+				
+				readRefType2();
+				
+				assert sig.charAt(idx-1) == ';' : "char=" + sig.charAt(idx-1) + ", idx=" +idx;
+			}
+			
+			private void readId()
+			{
+				char c;
+				while (Character.isJavaIdentifierPart((c = sig.charAt(idx))))
+				{
+					System.out.print(c);
+					++idx;
+				}
+			}
+			
+			private void readExtends2()
+			{
+				while (sig.charAt(idx) == ':')
+				{
+					++idx;
+				}
+				
+				System.out.print(" extends ");
+			}
+			
+			private void readRefType2()
+			{
+				readTypeName2();
+				
+				char c = sig.charAt(idx++);
+				if (c == '<')
+				{
+					System.out.print("<");
+					{
+						readGenList(0);
+					}
+					System.out.print(">");
+					
+					assert ((sig.charAt(idx-1) == ';') || (sig.charAt(idx-1) == '*')) : "char=" + sig.charAt(idx-1) + ", idx=" + idx;
+					
+					if (sig.charAt(idx) == '>')
+					{
+						//idx += 2;
+					}
+				}
+				else
+				{
+					int x = 1;
+					x = 2;
+				}
+			}
+			
+			private void readTypeName2()
+			{
+				char c;
+				while (((c = sig.charAt(idx)) != '*') && (c != ';') && (c != '<'))
+				{
+					System.out.print(c);
+					
+					++idx;
+				}
+			}
+			
+			private void readGenList(int recIdx)
+			{
+				if (recIdx > 0)
+				{
+					System.out.print(", ");
+				}
+				char c = sig.charAt(idx);
+				
+				if ((c == '+') || (c == '-') || (c == '*'))
+				{
+					//++idx;
+				}
+				
+				readRefType2();
+			
+				c = sig.charAt(idx);
+				
+				if (c != '>')
+				{
+					readGenList(recIdx + 1);
+					
+					
+				}
+				else
+				{
+					idx += 2;
+				}
+			}
+		}
+	}
 
     /**
      * The main method accepts the file(s) for verification. Multiple files can
@@ -42,29 +376,8 @@ public class Main {
 		//
 		try
 		{
-			com.telerik.java.api.bcel.Generator.ClassCallback cc = new com.telerik.java.api.bcel.Generator.ClassCallback()
-			{
-				@Override
-				public void onVisit(org.apache.bcel.classfile.JavaClass javaClass) {
-					System.out.println(javaClass.getClassName());
-				}
-			};
-			com.telerik.java.api.bcel.Generator.MethodCallback mc = new com.telerik.java.api.bcel.Generator.MethodCallback()
-			{
-				@Override
-				public void onVisit(org.apache.bcel.classfile.Method method) {
-					System.out.println(method.getName() + " " + method.getSignature());
-					
-				}
-			};
-			com.telerik.java.api.bcel.Generator.FieldCallback fc = new com.telerik.java.api.bcel.Generator.FieldCallback()
-			{
-				@Override
-				public void onVisit(org.apache.bcel.classfile.Field field) {
-					System.out.println(field.getName() + " " + field.getSignature());
-				}
-			};
-			com.telerik.java.api.bcel.Generator g = new com.telerik.java.api.bcel.Generator(cc, mc, fc);
+			DtsWriter dtsWriter = new DtsWriter();
+			com.telerik.java.api.bcel.Generator g = new com.telerik.java.api.bcel.Generator(dtsWriter);
 			g.visit("C:/Work/NativeScript/android-metadata-generator/jars");
 		}
 		catch (Exception ex)
--- src/JavaTSAPI/src/com/telerik/java/api/bcel/Generator.java
+++ src/JavaTSAPI/src/com/telerik/java/api/bcel/Generator.java
@@ -8,54 +8,47 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.InvalidParameterException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+import org.apache.bcel.Repository;
 import org.apache.bcel.classfile.ClassParser;
 import org.apache.bcel.classfile.Field;
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.Method;
+import org.apache.bcel.util.ClassPath;
+import org.apache.bcel.util.SyntheticRepository;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 
 public class Generator {
 	
-	public interface ClassCallback
+	public interface VisitorCallback
 	{
-		void onVisit(org.apache.bcel.classfile.JavaClass javaClass);
-	}
-	
-	public interface MethodCallback
-	{
+		void onEnter(String packageName);
+		void onLeave(String packageName);
+		void onEnter(org.apache.bcel.classfile.JavaClass javaClass);
+		void onLeave(org.apache.bcel.classfile.JavaClass javaClass);
 		void onVisit(org.apache.bcel.classfile.Method method);
-	}
-	
-	public interface FieldCallback
-	{
 		void onVisit(org.apache.bcel.classfile.Field field);
 	}
-
-
-	public Generator(ClassCallback classVisitor, MethodCallback methodVisitor, FieldCallback fieldVisitor)
+	
+	public Generator(VisitorCallback visitor)
 	{
-		if (classVisitor == null)
+		if (visitor == null)
 		{
-			throw new InvalidParameterException("classVisitor cannot be null");
+			throw new InvalidParameterException("visitor cannot be null");
 		}
-		if (methodVisitor == null)
-		{
-			throw new InvalidParameterException("methodVisitor cannot be null");
-		}
-		if (fieldVisitor == null)
-		{
-			throw new InvalidParameterException("fieldVisitor cannot be null");
-		}
-		this.classVisitor = classVisitor;
-		this.methodVisitor = methodVisitor;
-		this.fieldVisitor = fieldVisitor;
+		this.visitor = visitor;
 	}
 	
-	public void visit(String filepath) throws FileNotFoundException, IOException, InvalidParameterException
+	public void visit(String filepath) throws FileNotFoundException, IOException, InvalidParameterException, ClassNotFoundException
 	{
 		File f = new File(filepath);
 		
@@ -88,15 +81,46 @@ public class Generator {
 		}
 	}
 	
-	private void visitDirectory(File dir) throws IOException
+	private static class FullPathComparator implements java.util.Comparator<File>
+	{
+		@Override
+		public int compare(File o1, File o2) {
+			String n1 = o1.getName();
+			String n2 = o2.getName();
+			if (n1.startsWith(n2))
+				return -1;
+			if (n2.startsWith(n1))
+				return 1;
+			int result = n1.compareTo(n2);
+			return result;
+		}
+	}
+	
+	private static final FullPathComparator fullPathComparator = new FullPathComparator();
+	
+	private void visitDirectory(File dir) throws IOException, ClassNotFoundException
 	{
 		File[] files = dir.listFiles();
+		
+		Arrays.sort(files, fullPathComparator);
+		
 		
 		for (File f: files)
 		{
 			if (f.isDirectory())
 			{
+				String packageName = f.getName();
+				
+				if (packageName.contains("-"))
+				{
+					continue;
+				}
+				
+				visitor.onEnter(packageName);
+				
 				visitDirectory(f);
+				
+				visitor.onLeave(packageName);
 			}
 			else
 			{
@@ -114,7 +138,7 @@ public class Generator {
 		}
 	}
 	
-	private void visitJarFile(File jarFile) throws IOException
+	private void visitJarFile(File jarFile) throws IOException, ClassNotFoundException
 	{
 		//System.out.println(jarFile.getAbsolutePath());
 				
@@ -140,24 +164,30 @@ public class Generator {
 	}
 	
 
-	private void visitClassFile(File classFile) throws IOException
+	private void visitClassFile(File classFile) throws IOException, ClassNotFoundException
 	{
 		ClassParser cp = new ClassParser(classFile.getAbsolutePath());
 		
 		JavaClass clazz = cp.parse();
 		
-		if (clazz.isPublic() && !clazz.isSynthetic())
+		if ((clazz.isPublic() || clazz.isProtected()) && !clazz.isSynthetic())
 		{
+			if (clazz.getClassName().contains("-"))
+			{
+				return;
+			}
 			//System.out.println(clazz.getClassName());
 			
-			classVisitor.onVisit(clazz);
+			visitor.onEnter(clazz);
+			
+			// TODO: call recursively visitClassFile(for each nested subtype)
 			
 			Method[] methods = clazz.getMethods();
 			for (Method m: methods)
 			{
-				if (m.isPublic() || m.isProtected())
+				if ((m.isPublic() || m.isProtected()) && !m.isSynthetic())
 				{
-					methodVisitor.onVisit(m);
+					visitor.onVisit(m);
 				}
 			}
 
@@ -166,10 +196,11 @@ public class Generator {
 			{
 				if (f.isPublic() || f.isProtected())
 				{
-					fieldVisitor.onVisit(f);
+					visitor.onVisit(f);
 				}
 			}
-
+			
+			visitor.onLeave(clazz);
 		}
 	}
 
@@ -199,6 +230,8 @@ public class Generator {
 			}
 			else
 			{
+				newFile.getParentFile().getAbsoluteFile().mkdirs();
+				
 	   			FileOutputStream fos = null;
 	   
 				try
@@ -229,8 +262,6 @@ public class Generator {
        }
 	}    	
 	
-    private ClassCallback classVisitor;
-	private MethodCallback methodVisitor;
-	private FieldCallback fieldVisitor;
+    private VisitorCallback visitor;
 	
 }
